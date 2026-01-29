3475f9f45a6b38edb3601f653b004c4179b30ce0
Vinicius loch <viniciusdrloch@gmail.com>
2026-01-29 16:17:53 -0300

    Feat: Create about page and route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from '../pages/home'
 import Layout from '../components/layout'
+import About from '../pages/about'
 
 export function AppRoutes() {
   return (
@@ -8,6 +9,7 @@ export function AppRoutes() {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Layout>
     </BrowserRouter>
